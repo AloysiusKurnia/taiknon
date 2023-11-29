ab6704eee4bc556d55456fc4d8a9d4cf4ede03a5
Aloysius Kurnia Mahendra <aloysius1234@yahoo.co.id>
2023-11-29 08:32:48 +0700

    set username min count

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -9,9 +9,10 @@ export interface RegisterRequest {
 
 export enum RegisterError {
     UsernameTaken = 'Username is already taken.',
+    UsernameInvalid = 'The username can only contain letters, numbers, and dashes (-).',
+    UsernameTooShort = 'The username is too short. It must be at least 4 characters.',
     PasswordTooShort = 'The password is too short. It must be at least 6 characters.',
     PasswordTooLong = 'The password is too long. It must be at most 128 characters.',
-    UsernameInvalid = 'The username can only contain letters, numbers, and dashes (-).',
 }
 
 const usernameRegex = /^[A-Za-z0-9\-]+$/;
@@ -33,6 +34,12 @@ export async function POST(req: Request) {
         return new Response(null, {
             status: 400,
             statusText: RegisterError.PasswordTooLong
+        });
+    }
+    if (username.length < 4) {
+        return new Response(null, {
+            status: 400,
+            statusText: RegisterError.UsernameTooShort
         });
     }
 
